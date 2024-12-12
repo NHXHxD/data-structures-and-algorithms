d926575c2fb18cf5a81d51a46554051bbb3f2742
Hy Nguyen <hynguyen2004@gmail.com>
2024-12-12 15:03:58 +1030

    Hy

---
+++ Bellman-Ford.py
@@ -0,0 +1,13 @@
+def bellman_ford(vertices, edges, source):
+
+    dist = [float('inf')] * vertices
+    dist[source] = 0
+
+    for i in range(vertices - 1):
+        for (u, v, w) in edges:
+            if dist[u] != float('inf') and dist[u] + w < dist[v]:
+                dist[v] = dist[u] + w
+
+    return dist
+
+
